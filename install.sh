f64ef8a3ca9419857e50c8499ba7d7673f489cfc
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-17 02:54:40 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -48,7 +48,7 @@ elif [ $OS == "Debian" ] && [ $VER == "10" ]; then
     # Установка Docker CE для Debian 10
     apt-get update
     apt-get install -y apt-transport-https ca-certificates curl gnupg2 software-properties-common
-    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -y -
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
     apt-get update
     apt-get install -y docker-ce docker-ce-cli containerd.io
