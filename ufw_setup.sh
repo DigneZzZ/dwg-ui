c5a814702cf353df5c5038fb91aef870d99a7bd8
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-17 17:36:12 +0300

    Update ufw_setup.sh

--- ufw_setup.sh
+++ ufw_setup.sh
@@ -1,50 +1,41 @@
 #!/bin/bash
 
-# Определение цветов
-GREEN='\033[0;32m'
+# Определяем цвета для подсветки текста
 RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[0;33m'
 NC='\033[0m' # No Color
 
-# Определение операционной системы
-if grep -q "Ubuntu 22.04" /etc/os-release; then
-  echo -e "${GREEN}Установка UFW и NTP на Ubuntu 22.04${NC}"
-  sudo apt update
-  sudo apt install ufw -y
-  sudo apt install ntp -y
-elif grep -q "Ubuntu 20.04" /etc/os-release; then
-  echo -e "${GREEN}Установка UFW и NTP на Ubuntu 20.04${NC}"
-  sudo apt update
-  sudo apt install ufw -y
-  sudo apt install ntp -y
-elif grep -q "Debian GNU/Linux 11" /etc/os-release; then
-  echo -e "${GREEN}Установка UFW и NTP на Debian 11${NC}"
-  sudo apt update
-  sudo apt install ufw -y
-  sudo apt install ntp -y
-elif grep -q "Debian GNU/Linux 10" /etc/os-release; then
-  echo -e "${GREEN}Установка UFW и NTP на Debian 10${NC}"
-  sudo apt update
-  sudo apt install ufw -y
-  sudo apt install ntp -y
-elif grep -q "CentOS Linux 8" /etc/os-release; then
-  echo -e "${GREEN}Установка firewalld и chrony на CentOS 8${NC}"
-  sudo dnf install firewalld -y
-  sudo systemctl start firewalld
-  sudo systemctl enable firewalld
-  sudo dnf install chrony -y
-  sudo systemctl start chronyd
-  sudo systemctl enable chronyd
-elif grep -q "CentOS Linux 9" /etc/os-release; then
-  echo -e "${GREEN}Установка firewalld и chrony на CentOS 9${NC}"
-  sudo dnf install firewalld -y
-  sudo systemctl start firewalld
-  sudo systemctl enable firewalld
-  sudo dnf install chrony -y
-  sudo systemctl start chronyd
-  sudo systemctl enable chronyd
+# Проверяем наличие UFW на Debian или Ubuntu
+if [ -x "$(command -v ufw)" ]; then
+    echo -e "{GREEN}UFW уже установлен{NC}"
+# Если UFW не установлен, проверяем, является ли операционная система Debian или Ubuntu
+elif [ -x "$(command -v apt-get)" ]; then
+    echo -e "{YELLOW}UFW не установлен{NC}"
+    echo -e "{YELLOW}Установка UFW...{NC}"
+    # Добавляем возможность установки на системах Ubuntu 20.04, Ubuntu 22.04, Debian 10, Debian 11
+    if [ $(lsb_release -rs) == "20.04" ] || [ $(lsb_release -rs) == "22.04" ] || [ $(lsb_release -rs) == "10" ] || [ $(lsb_release -rs) == "11" ]; then
+        sudo apt-get install ufw -y
+        echo -e "{GREEN}Установка UFW завершена успешно{NC}"
+    else
+        echo -e "{RED}Неподдерживаемая версия операционной системы.{NC}"
+    fi
+# Проверяем наличие firewalld на CentOS
+elif [ -x "$(command -v firewall-cmd)" ]; then
+    echo -e "{GREEN}firewalld уже установлен{NC}"
+# Если firewalld не установлен, проверяем, является ли операционная система CentOS
+elif [ -x "$(command -v yum)" ]; then
+    echo -e "{YELLOW}firewalld не установлен{NC}"
+    echo -e "{YELLOW}Установка firewalld...{NC}"
+    # Добавляем возможность установки на системах CentOS 7, CentOS 8, CentOS 9
+    if [ $(cat /etc/redhat-release | grep -oE '[0-9]+' | head -1) == "7" ] || [ $(cat /etc/redhat-release | grep -oE '[0-9]+' | head -1) == "8" ] || [ $(cat /etc/redhat-release | grep -oE '[0-9]+' | head -1) == "9" ]; then
+        sudo yum install firewalld -y
+        echo -e "{GREEN}Установка firewalld завершена успешно{NC}"
+    else
+        echo -e "{RED}Неподдерживаемая версия операционной системы.{NC}"
+    fi
 else
-  echo -e "${RED}Не удалось определить операционную систему${NC}"
-  exit 1
+    echo -e "{RED}Операционная система не поддерживается.{NC}"
 fi
 
 # Считываем ssh порт из файла sshd_config
@@ -56,22 +47,12 @@ read use_default_port
 
 if [[ "$use_default_port" =~ ^(y|Y) ]]; then
   # Добавляем ssh порт в список разрешенных
-  if grep -q "Ubuntu" /etc/os-release; then
-    sudo ufw allow $ssh_port/tcp
-  else
-    sudo firewall-cmd --add-port=$ssh_port/tcp --permanent
-    sudo firewall-cmd --reload
-  fi
+  sudo ufw allow $ssh_port/tcp
 else
   echo "Введите ssh порт:"
   read custom_ssh_port
   # Добавляем кастомный ssh порт в список разрешенных
-  if grep -q "Ubuntu" /etc/os-release; then
-    sudo ufw allow $custom_ssh_port/tcp
-  else
-    sudo firewall-cmd --add-port=$custom_ssh_port/tcp --permanent
-    sudo firewall-cmd --reload
-  fi
+  sudo ufw allow $custom_ssh_port/tcp
 fi
 
 # Предлагаем пользователю добавить разрешенные порты для других сервисов
@@ -86,12 +67,7 @@ if [[ "$add_additional_ports" =~ ^(y|Y) ]]; then
   do
     echo "Введите порт для сервиса $service:"
     read port
-    if grep -q "Ubuntu" /etc/os-release; then
-      sudo ufw allow $port/tcp
-    else
-      sudo firewall-cmd --add-port=$port/tcp --permanent
-      sudo firewall-cmd --reload
-    fi
+    sudo ufw allow $port/tcp
   done
 
   echo -e "${GREEN}Разрешенные порты для сервисов добавлены.${NC}"
@@ -100,10 +76,5 @@ else
 fi
 
 # Включаем firewall
-if grep -q "Ubuntu" /etc/os-release; then
-  sudo ufw enable
-  echo -e "${GREEN}Настройка UFW завершена.${NC}"
-else
-  sudo systemctl enable firewalld
-  echo -e "${GREEN}Настройка firewalld завершена.${NC}"
-fi
+sudo ufw enable
+echo -e "${GREEN}Настройка UFW завершена.${NC}"
