8e162a4eae47d221f37c57249491dfc709c1a836
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-17 11:33:43 +0300

    Update ufw-install.sh

--- ufw-install.sh
+++ ufw-install.sh
@@ -1,10 +1,15 @@
-
 #!/bin/bash
+
+# Цвета текста
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
 
 # Проверка наличия установленного ufw
 if [ -x "$(command -v ufw)" ]; then
   echo "UFW уже установлен."
-  read -p "Хотите обновить список портов на основе используемых сервисов? (y/n): " update_ports
+  read -p $'\e[1;33m'"Хотите обновить список портов на основе используемых сервисов? (y/n): "'\e[0m' update_ports
   if [ "$update_ports" = "y" ]; then
     # Получить список открытых портов с помощью netstat и фильтрацией через awk и sort
     ports=$(sudo netstat -lnp | awk '/^tcp/ {if ($NF != "LISTENING") next; split($4, a, ":"); print a[2]}' | sort -u)
@@ -20,12 +25,12 @@ if [ -x "$(command -v ufw)" ]; then
 
     # Сохранение правил и включение ufw
     sudo ufw enable
-    echo "Список портов был успешно обновлен."
+    echo -e "${GREEN}Список портов был успешно обновлен.${NC}"
   else
     echo "Установка завершена без изменений."
   fi
 else
-  read -p "UFW не установлен, хотите установить его? (y/n): " install_ufw
+  read -p $'\e[1;33m'"UFW не установлен, хотите установить его? (y/n): "'\e[0m' install_ufw
   if [ "$install_ufw" = "y" ]; then
     # Установка ufw
     sudo apt-get update
@@ -45,7 +50,7 @@ else
 
     # Сохранение правил и включение ufw
     sudo ufw enable
-    echo "UFW был успешно установлен и настроен на открытие используемых портов."
+    echo -e "${GREEN}UFW был успешно установлен и настроен на открытие используемых портов.${NC}"
   else
     echo "Установка завершена без изменений."
   fi
