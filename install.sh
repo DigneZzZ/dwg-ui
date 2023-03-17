4205f0e9eedb1d853f4b324b7efd9b9eaa5f1830
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-17 13:05:52 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,55 +7,62 @@ RED='\033[0;31m'
 NC='\033[0m'
 
 # Проверяем, выполняется ли скрипт от имени пользователя root
-if [ "$EUID" -ne 0 ]
-  then echo -e "${RED}Запустите скрипт с правами root${NC}"
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${RED}Запустите скрипт с правами root${NC}"
   exit
 fi
 
 # Проверяем, установлен ли Docker
 if [ -x "$(command -v docker)" ]; then
-    echo -e "${GREEN}Docker уже установлен${NC}"
+  echo -e "${GREEN}Docker уже установлен${NC}"
 else
-    # Проверяем, какое распределение используется, и устанавливаем необходимые зависимости
-    if [ -f /etc/debian_version ]; then
-        apt-get update
-        apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
-    elif [ -f /etc/redhat-release ]; then
-        dnf install -y dnf-plugins-core
-        dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-        dnf install -y curl
-    else
-        echo -e "${RED}Неподдерживаемое распределение${NC}"
-        exit
-    fi
+  # Проверяем, какое распределение используется, и устанавливаем необходимые зависимости
+  if [ -f /etc/debian_version ]; then
+    apt-get update
+    apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
+  elif [ -f /etc/redhat-release ]; then
+    dnf install -y dnf-plugins-core
+    dnf config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+    dnf install -y curl
+  else
+    echo -e "${RED}Неподдерживаемое распределение${NC}"
+    exit
+  fi
 
-    # Устанавливаем Docker
-    curl -fsSL https://get.docker.com -o get-docker.sh
-    sh get-docker.sh
+  # Устанавливаем Docker
+  curl -fsSL https://get.docker.com -o get-docker.sh
+  sh get-docker.sh
 
-    # Запускаем и включаем службу Docker
-    systemctl start docker
-    systemctl enable docker
+  # Запускаем и включаем службу Docker
+  systemctl start docker
+  systemctl enable docker
 
-    echo -e "${GREEN}Docker успешно установлен${NC}"
+  echo -e "${GREEN}Docker успешно установлен${NC}"
 fi
 
 # Устанавливаем Docker Compose
 LATEST_VERSION=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | grep "tag_name" | cut -d '"' -f 4)
 if [ -x "$(command -v docker-compose)" ]; then
-    INSTALLED_VERSION=$(docker-compose version --short)
-    if [ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]; then
-        echo -e "${GREEN}Установлена последняя версия Docker Compose${NC}"
-    else
-        echo -e "${YELLOW}Обнаружена устаревшая версия Docker Compose${NC}"
-        read -p "Хотите обновить Docker Compose? (y/n) " update_docker_compose
-        case $update_docker_compose in
-            [Yy]* ) sudo rm /usr/local/bin/docker-compose && sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && echo -e "${GREEN}Docker Compose успешно обновлен${NC}";;
-            [Nn]* ) exit;;
-        esac
-    fi
+  INSTALLED_VERSION=$(docker-compose version --short)
+  if [ "$LATEST_VERSION" == "$INSTALLED_VERSION" ]; then
+    echo -e "${GREEN}Установлена последняя версия Docker Compose${NC}"
+  else
+    echo -e "${YELLOW}Обнаружена устаревшая версия Docker Compose${NC}"
+    read -p "Хотите обновить Docker Compose? (y/n) " update_docker_compose
+    case $update_docker_compose in
+      [Yy]* ) 
+        sudo rm /usr/local/bin/docker-compose && sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && echo -e "${GREEN}Docker Compose успешно обновлен${NC}"
+        ;;
+      [Nn]* ) 
+        echo -e "${YELLOW}Продолжаем выполнение скрипта без обновления Docker Compose${NC}"
+        ;;
+      * ) 
+        echo -e "${RED}Неправильный ввод. Продолжаем выполнение скрипта без обновления Docker Compose${NC}"
+        ;;
+    esac
+  fi
 else
-    sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && echo -e "${GREEN}Docker Compose успешно установлен${NC}"
+  sudo curl -L "https://github.com/docker/compose/releases/download/$LATEST_VERSION/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose && sudo chmod +x /usr/local/bin/docker-compose && echo -e "${GREEN}Docker Compose успешно установлен${NC}"
 fi
 
 # Устанавливаем редактор Nano
