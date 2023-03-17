ad9715aadea63af01f9104fa4e6bd6d51dbcea7b
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-17 04:46:17 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -173,23 +173,54 @@ else
     echo "Файл /etc/ssh/sshd_config не найден."
 fi
 
-# Проверка наличия UFW
-if ! command -v ufw &> /dev/null
-then
-    read -p "UFW не найден. Установить UFW? (y/n) " INSTALL_UFW
-    if [ "$INSTALL_UFW" == "y" ]; then
-        # Установка UFW
-        apt-get update
-        apt-get install -y ufw
-        # Открытие портов
-        ufw allow ssh
-        ufw allow http
-        ufw allow https
-        ufw enable
-    fi
+#!/bin/bash
+
+# Проверка наличия установленного ufw
+if [ -x "$(command -v ufw)" ]; then
+  echo "UFW уже установлен."
+  read -p "Хотите обновить список портов на основе используемых сервисов? (y/n): " update_ports
+  if [ "$update_ports" = "y" ]; then
+    # Получить список открытых портов с помощью netstat и фильтрацией через awk и sort
+    ports=$(sudo netstat -lnp | awk '/^tcp/ {if ($NF != "LISTENING") next; split($4, a, ":"); print a[2]}' | sort -u)
+
+    # Очистка текущих правил и настройка новых правил
+    sudo ufw --force reset
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    for port in $ports
+    do
+      sudo ufw allow $port/tcp
+    done
+
+    # Сохранение правил и включение ufw
+    sudo ufw enable
+    echo "Список портов был успешно обновлен."
+  else
+    echo "Установка завершена без изменений."
+  fi
 else
-    echo "UFW уже установлен."
-    # Проверка открытых портов
-    echo "Открытые порты:"
-    ufw status
+  read -p "UFW не установлен, хотите установить его? (y/n): " install_ufw
+  if [ "$install_ufw" = "y" ]; then
+    # Установка ufw
+    sudo apt-get update
+    sudo apt-get install ufw
+
+    # Получить список открытых портов с помощью netstat и фильтрацией через awk и sort
+    ports=$(sudo netstat -lnp | awk '/^tcp/ {if ($NF != "LISTENING") next; split($4, a, ":"); print a[2]}' | sort -u)
+
+    # Очистка текущих правил и настройка новых правил
+    sudo ufw --force reset
+    sudo ufw default deny incoming
+    sudo ufw default allow outgoing
+    for port in $ports
+    do
+      sudo ufw allow $port/tcp
+    done
+
+    # Сохранение правил и включение ufw
+    sudo ufw enable
+    echo "UFW был успешно установлен и настроен на открытие используемых портов."
+  else
+    echo "Установка завершена без изменений."
+  fi
 fi
