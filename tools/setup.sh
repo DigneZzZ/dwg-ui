78cbaf7c431ae8e50a2b814a6af86198311a3116
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-19 16:37:36 +0300

    Update setup.sh

--- tools/setup.sh
+++ tools/setup.sh
@@ -1,53 +1,97 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-YELLOW='\033[1;33m'
-NC='\033[0m'
+# Проверяем наличие утилит curl, chmod и read
+if ! command -v curl &> /dev/null || ! command -v chmod &> /dev/null || ! command -v read &> /dev/null
+then
+    echo "Ошибка: Не найдены необходимые утилиты curl, chmod и read. Пожалуйста, установите их и попробуйте снова"
+    exit 1
+fi
 
-echo -e "${YELLOW}Добро пожаловать в утилиту загрузки и запуска скриптов!${NC}"
-echo -e "${YELLOW}Выберите файлы для загрузки (разделите номера запятыми):${NC}"
-
-echo -e "${YELLOW}1. ssh.sh - Изменение порта для SSH. (не забудьте на что меняли!!!) - запускайте раньше чем установка UFW"
-echo -e "2. ufw-install.sh - Установка UFW (FirewallD для CentOS) и настройка портов"
-echo -e "3. ufw-docker.sh - Установка ufw-docker для безопасности контейнеров Docker (нет поддержки CentOS)${NC}"
-
-read -p "Введите свой выбор (например, 1,2,3): " choices
-
-# Разбиваем введенную строку на массив, используя запятые в качестве разделителей
-IFS=',' read -ra selected <<< "$choices"
-
-# Проходим по выбранным номерам и загружаем соответствующие файлы
-for choice in "${selected[@]}"; do
-    case $choice in
-        1)
-            if curl -O https://raw.githubusercontent.com/DigneZzZ/ad-wireguard/main/ssh.sh && chmod +x ssh.sh && sh ssh.sh; then
-                echo -e "${GREEN}Файл ssh.sh загружен и запущен успешно.${NC}"
-            else
-                echo -e "${RED}Не удалось загрузить или запустить файл ssh.sh${NC}"
-            fi
-            ;;
-        2)
-            if curl -O https://raw.githubusercontent.com/DigneZzZ/ad-wireguard/main/ufw-setup.sh -o ufw-setup.sh && chmod +x ufw-setup.sh && sh ufw-setup.sh; then
-                echo -e "${GREEN}Файл ufw-setup.sh загружен и запущен успешно.${NC}"
-            else
-                echo -e "${RED}Не удалось загрузить или запустить файл ufw-setup.sh.${NC}"
-            fi
-            ;;
-            
-        3)
-            if curl -O https://raw.githubusercontent.com/DigneZzZ/ad-wireguard/main/ufw-docker.sh && chmod +x ufw-docker.sh && sh ufw-docker.sh; then
-                echo -e "${GREEN}Файл ufw-docker.sh загружен и запущен успешно.${NC}"
-            else
-                echo -e "${RED}Не удалось загрузить или запустить файл ufw-docker.sh.${NC}"
-            fi
-            ;;
-            
-        *)
-            echo -e "${RED}Неверный выбор: $choice${NC}"
-            ;;
-    esac
+# Проверяем наличие файлов на сервере
+for i in {1..5}
+do
+    if curl --head --silent --fail https://example.com/script${i}.sh 2>&1 | grep "HTTP/1.[01] [23].." > /dev/null
+    then
+        echo "Файл script${i}.sh найден на сервере"
+    else
+        echo "Ошибка: Файл script${i}.sh не найден на сервере"
+        exit 1
+    fi
 done
 
-echo -e "${YELLOW}Загрузка и запуск завершены.${NC}"
-echo -e "${GREEN}http://openode.ru${NC}"
+# Предлагаем пользователю выбрать скрипты для установки
+printf "Выберите скрипты для установки:\n"
+printf "1. Скрипт 1\n"
+printf "2. Скрипт 2\n"
+printf "3. Скрипт 3\n"
+printf "4. Скрипт 4\n"
+printf "5. Скрипт 5\n"
+printf "Введите номер скрипта (через запятую, например, 1,2,3): "
+read -r scripts
+
+# Извлекаем выбранные скрипты в массив
+IFS=',' read -ra script_arr <<< "$scripts"
+
+# Итерируемся по массиву и устанавливаем выбранные скрипты
+for script_num in "${script_arr[@]}"
+do
+  case $script_num in
+    1)
+      # Скачиваем и устанавливаем первый скрипт
+      printf "\033[0;32mУстановка скрипта 1...\033[0m\n"
+      curl -O https://example.com/script1.sh && chmod +x script1.sh && ./script1.sh
+      if [ $? -eq 0 ]; then
+        printf "\033[0;32mСкрипт 1 успешно установлен\033[0m\n"
+      else
+        printf "\033[0;31mНе удалось установить скрипт 1\033[0m\n"
+      fi
+      ;;
+    2)
+      # Скачиваем и устанавливаем второй скрипт
+      printf "\033[0;32mУстановка скрипта 2...\033[0m\n"
+      curl -O https://example.com/script2.sh && chmod +x script2.sh && ./script2.sh
+      if [ $? -eq 0 ]; then
+        printf "\033[0;32mСкрипт 2 успешно установлен\033[0m\n"
+      else
+        printf "\033[0;31mНе удалось установить скрипт 2\033[0m\n"
+      fi
+      ;;
+    3)
+    
+      # Скачиваем и устанавливаем третий
+            printf "\033[0;32mУстановка скрипта 3...\033[0m\n"
+      curl -O https://example.com/script3.sh && chmod +x script3.sh && ./script3.sh
+      if [ $? -eq 0 ]; then
+        printf "\033[0;32mСкрипт 3 успешно установлен\033[0m\n"
+      else
+        printf "\033[0;31mНе удалось установить скрипт 3\033[0m\n"
+      fi
+      ;;
+    4)
+      # Скачиваем и устанавливаем четвертый скрипт
+      printf "\033[0;32mУстановка скрипта 4...\033[0m\n"
+      curl -O https://example.com/script4.sh && chmod +x script4.sh && ./script4.sh
+      if [ $? -eq 0 ]; then
+        printf "\033[0;32mСкрипт 4 успешно установлен\033[0m\n"
+      else
+        printf "\033[0;31mНе удалось установить скрипт 4\033[0m\n"
+      fi
+      ;;
+    5)
+      # Скачиваем и устанавливаем пятый скрипт
+      printf "\033[0;32mУстановка скрипта 5...\033[0m\n"
+      curl -O https://example.com/script5.sh && chmod +x script5.sh && ./script5.sh
+      if [ $? -eq 0 ]; then
+        printf "\033[0;32mСкрипт 5 успешно установлен\033[0m\n"
+      else
+        printf "\033[0;31mНе удалось установить скрипт 5\033[0m\n"
+      fi
+      ;;
+    *)
+      # Некорректный ввод
+      printf "\033[0;33mНекорректный ввод. Попробуйте снова.\033[0m\n"
+      ;;
+  esac
+done
+
+      
