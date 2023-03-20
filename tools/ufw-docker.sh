95d1434e79e032244677bef2f2d10963b136aceb
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-21 00:30:04 +0300

    Update ufw-docker.sh

--- tools/ufw-docker.sh
+++ tools/ufw-docker.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+# Скачиваем ufw-docker
 sudo wget -O /usr/local/bin/ufw-docker https://github.com/chaifeng/ufw-docker/raw/master/ufw-docker
+
+# Даем права на выполнение
 sudo chmod +x /usr/local/bin/ufw-docker
+
+# Устанавливаем ufw-docker
 ufw-docker install
-sudo systemctl restart ufw
-ufw route allow proto tcp from 10.10.10.0/24 to any port 51821
-ufw disable
-ufw enable -y
-sudo systemctl restart ufw
+
+# Разрешаем трафик на порт 51821 для сети 10.10.10.0/24
+sudo ufw route allow proto tcp from 10.10.10.0/24 to any port 51821
+
+# Отключаем ufw
+sudo ufw disable
+
+# Включаем ufw и пропускаем все запросы подтверждения
+sudo ufw --force enable
