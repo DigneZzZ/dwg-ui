2ad4e120f8adee92004e7f673b1d8f57cb53daf8
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-25 15:14:36 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,30 +15,40 @@ printf "\e[42mКлонирование репозитория ad-wireguard...\e[
 git clone https://github.com/dignezzz/ad-wireguard.git
 printf "\e[42mРепозиторий ad-wireguard успешно склонирован.\e[0m\n"
 
+# Переходим в папку ad-wireguard
+printf "\e[42mПереходим в папку ad-wireguard...\e[0m\n"
+cd ad-wireguard
+printf "\e[42mПерешли в папку ad-wireguard\e[0m\n"
+
 # Установка прав на файл установки
 printf "\e[42mУстановка прав на файл установки...\e[0m\n"
-chmod +x ad-wireguard/install.sh
+chmod +x install.sh
 printf "\e[42mПрава на файл установки выставлены.\e[0m\n"
 
 # Запуск установки
 printf "\e[42mЗапуск установки ad-wireguard...\e[0m\n"
-./ad-wireguard/install.sh
+./install.sh
 printf "\e[42mУстановка ad-wireguard успешно завершена.\e[0m\n"
 
 # Установка прав на директорию tools
 printf "\e[42mУстановка прав на директорию tools...\e[0m\n"
-chmod +x -R ad-wireguard/tools
+chmod +x -R tools
 printf "\e[42mПрава на директорию tools успешно установлены.\e[0m\n"
 
 # Запуск скрипта ssh.sh
 printf "\e[42mЗапуск скрипта ssh.sh для смены стандартного порта SSH...\e[0m\n"
-./ad-wireguard/tools/ssh.sh
+./tools/ssh.sh
 printf "\e[42mСкрипт ssh.sh успешно выполнен.\e[0m\n"
 
 # Запуск скрипта ufw.sh
 printf "\e[42mЗапуск скрипта ufw.sh для установки UFW Firewall...\e[0m\n"
-./ad-wireguard/tools/ufw.sh
+./tools/ufw.sh
 printf "\e[42mСкрипт ufw.sh успешно выполнен.\e[0m\n"
+
+# Переходим в папку /
+printf "\e[42mПереходим в папку /root/...\e[0m\n"
+cd
+printf "\e[42mПерешли в папку /root/ \e[0m\n"
 
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
 printf '\e[48;5;202m\e[30m Всё установлено! \e[0m\n'
