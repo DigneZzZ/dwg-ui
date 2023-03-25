cd3afd8598e0f523344917365a6b4eb1eb4a787f
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-03-25 12:31:07 +0300

    Update swap.sh

--- tools/swap.sh
+++ tools/swap.sh
@@ -2,27 +2,36 @@
 
 # Проверить, существует ли файл подкачки /swapfile
 if grep -q /swapfile /etc/fstab; then
-    echo "Файл подкачки уже существует."
-    current_swap_size=$(grep /swapfile /proc/swaps | awk '{print $3}')
-    echo "Текущий размер подкачки: $current_swap_size"
-    # Используем флаг -y, чтобы удалить запись из /etc/fstab без запроса подтверждения
-    sudo sed -i -e '/\/swapfile/{/swapfile/!d;}' -e '/\/swapfile/d' -y /etc/fstab
+    printf "Файл подкачки уже существует.\n"
+    currentswapsize=$(grep /swapfile /proc/swaps | awk '{print $3}')
+    printf "Текущий размер подкачки: %s\n" "$currentswapsize"
+    printf "Вы хотите создать новый файл подкачки? (y/n) " 
+    read -r choice
+    case "$choice" in
+        y|Y ) ;;
+        n|N ) exit;;
+         ) printf "Неправильный выбор. Отмена.\n"; exit;;
+    esac
+    # Удалить старый файл подкачки и запись из /etc/fstab
+    sudo swapoff /swapfile
+    sudo rm /swapfile
+    sudo sed -i '/\/swapfile/d' /etc/fstab
 fi
 
 # Получить размер ОЗУ в байтах
-mem_bytes=$(grep MemTotal /proc/meminfo | awk '{print $2 * 1024}')
+mem_bytes=$(grep MemTotal /proc/meminfo | awk '{print $2  1024}')
 
 # Рассчитать размер файла подкачки
-if (( $mem_bytes < 2147483648 )); then
+if (( membytes < 2147483648 )); then
     # Вдвое больше объема ОЗУ, если ОЗУ менее 2 ГБ
-    swap_size=$((mem_bytes * 2))
+    swapsize=$(( membytes * 2 ))
 else
     # ОЗУ больше или равно 2 ГБ, размер подкачки равен ОЗУ + 2 ГБ
-    swap_size=$((mem_bytes + 2147483648))
+    swapsize=$(( membytes + 2147483648 ))
 fi
 
 # Создать файл подкачки
-sudo fallocate -l $swap_size /swapfile
+sudo fallocate -l "$swapsize" /swapfile
 
 # Назначить правильные разрешения на файл подкачки
 sudo chmod 600 /swapfile
@@ -34,4 +43,4 @@ sudo mkswap /swapfile
 sudo swapon /swapfile
 
 # Добавить запись в /etc/fstab для автоматического включения файла подкачки при загрузке системы
-echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+printf '/swapfile none swap sw 0 0\n' | sudo tee -a /etc/fstab
