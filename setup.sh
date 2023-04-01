6c128e00710286afeb39fd75a06ede6e7a10b03e
Alexey <50312583+DigneZzZ@users.noreply.github.com>
2023-04-01 15:01:35 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,24 +15,24 @@ printf "\e[42mGit успешно установлен.\e[0m\n"
 printf "\e[42mКлонирование репозитория ad-wireguard...\e[0m\n"
 git clone https://github.com/dignezzz/ad-wireguard.git temp
 
-if [ ! -d "ad-wireguard" ]; then
-  mkdir ad-wireguard
-  echo "Папка ad-wireguard создана."
+if [ ! -d "dwg-ui" ]; then
+  mkdir dwg-ui
+  echo "Папка dwg-ui создана."
 else
-  echo "Папка ad-wireguard уже существует."
+  echo "Папка dwg-ui уже существует."
 fi
 
 # копирование содержимого временной директории в целевую директорию с перезаписью существующих файлов и папок
-cp -rf temp/* ad-wireguard/
+cp -rf temp/* dwg-ui/
 
 # удаление временной директории со всем ее содержимым
 rm -rf temp
-printf "\e[42mРепозиторий ad-wireguard успешно клонирован до актуальной версии в репозитории.\e[0m\n"
+printf "\e[42mРепозиторий dwg-ui успешно клонирован до актуальной версии в репозитории.\e[0m\n"
 
 # Переходим в папку ad-wireguard
-printf "\e[42mПереходим в папку ad-wireguard...\e[0m\n"
-cd ad-wireguard
-printf "\e[42mПерешли в папку ad-wireguard\e[0m\n"
+printf "\e[42mПереходим в папку dwg-ui...\e[0m\n"
+cd dwg-ui
+printf "\e[42mПерешли в папку dwg-ui\e[0m\n"
 
 # Установка прав на файл установки
 printf "\e[42mУстановка прав на файл установки...\e[0m\n"
@@ -70,5 +70,5 @@ printf "\e[48;5;202m\e[30m Адрес входа в веб-интерфейс Wi
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
 printf '\e[48;5;202m\e[30m Не забудь отдельно установить UFW-Docker, для закрытия веб-интерфейса wireguard. \e[0m\n'
 printf '\e[48;5;196m\e[97m ВНИМАНИЕ! Запускать только после того как создадите для себя клиента в WireGUARD!!! \e[0m\n'
-printf '\e[48;5;202m\e[30m команда для установки: ./ad-wireguard/tools/ufw-docker.sh \e[0m\n'
+printf '\e[48;5;202m\e[30m команда для установки: ./dwg-ui/tools/ufw-docker.sh \e[0m\n'
 printf '\e[48;5;202m\e[30m ################################################################## \e[0m\n'
